284244837b2e8413c5c575332cc4f766758653b7
NahumMatus <nahummatus2016@udec.cl>
2023-04-04 13:58:56 -0400

    add interfaces and headers response

--- src/indicator/indicator.controller.ts
+++ src/indicator/indicator.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { IndicatorService } from './indicator.service';
+import { Indicators } from './interfaces/indicators.interface';
 
 @Controller('indicator')
 export class IndicatorController {
   constructor(private indicatorSvc: IndicatorService) {}
   @Get()
-  getBitcoinPrice() {
-    return this.indicatorSvc.getIndicators();
+  async getBitcoinPrice() {
+    const indicators: Indicators = await this.indicatorSvc.getIndicators();
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+      },
+      body: indicators,
+    };
+    return response;
   }
 }
--- src/indicator/indicator.service.ts
+++ src/indicator/indicator.service.ts
@@ -1,19 +1,21 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { map, catchError } from 'rxjs';
+import { map, catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { Indicators } from './interfaces/indicators.interface';
 
 @Injectable()
 export class IndicatorService {
   constructor(private http: HttpService) {}
 
-  async getIndicators() {
-    return this.http
-      .get('https://mindicador.cl/api')
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError(() => {
-          throw new ForbiddenException('API not available');
+  async getIndicators(): Promise<Indicators> {
+    const { data } = await firstValueFrom(
+      this.http.get<Indicators>('https://mindicador.cl/api').pipe(
+        catchError((error: AxiosError) => {
+          throw 'An error happened!';
         }),
-      );
+      ),
+    );
+    return data;
   }
 }
